ca2d9bba6a05109cce00712bb2bce4a2ee468cfe
Robin Northcott <robinintheuk@mac.com>
2016-09-26 18:26:22 +0200

    small changes

--- downloadData.R
+++ downloadData.R
@@ -2,7 +2,7 @@
 
 file_list <- c(paste("train_",1:3,".zip",sep=""),paste("test_",1:3,".zip",sep=""))
 url <- "https://www.kaggle.com/c/melbourne-university-seizure-prediction/download/"
-download_directory <- "./download"
+download_directory <- "./download/"
 
 #checks to see if files are already here
 file_list <- file_list[!(file_list %in% paste(dir(download_directory),".zip",sep=""))]
@@ -11,7 +11,7 @@ file_list <- file_list[!(file_list %in% paste(dir(download_directory),".zip",sep
 for(file in file_list){
   if(!(file %in% dir(download_directory))) download.file(
     paste(url,file,sep=""),destfile=paste(download_directory,file,sep=""),
-    method="wget")
+    method="wget", extra = "--load-cookies cookies.txt -a ./download.log")
 }
 
 # unzip the files and delete the .zip files
