736111fb3f225bbc969751703f96f7946ec52ab4
robin <robinintheuk@mac.com>
2016-09-27 05:41:34 +0000

    download script finished

--- downloadData.R
+++ downloadData.R
@@ -1,21 +1,24 @@
 # Downloads competition data files from Kaggle
 
-file_list <- c(paste("train_",1:3,".zip",sep=""),paste("test_",1:3,".zip",sep=""))
+file_list <- c(paste("train_",2:3,sep=""),paste("test_",1:3,sep=""))
 url <- "https://www.kaggle.com/c/melbourne-university-seizure-prediction/download/"
 download_directory <- "./download/"
-
-#checks to see if files are already here
-file_list <- file_list[!(file_list %in% paste(dir(download_directory),".zip",sep=""))]
+s3_uri <- "s3://robinrdata/melbourne-university-seizure-prediction/download/"
 
 # download any files not present
 for(file in file_list){
-  if(!(file %in% dir(download_directory))) download.file(
-    paste(url,file,sep=""),destfile=paste(download_directory,file,sep=""),
+  download.file(
+    paste(url,file,".zip",sep=""),destfile=paste(download_directory,file,".zip",sep=""),
     method="wget", extra = "--load-cookies cookies.txt -a ./download.log")
+  unzip(paste(download_directory,file,".zip",sep=""), exdir=download_directory, unzip="unzip")
+  file.remove(paste(download_directory,file,".zip",sep=""))
+  
+  cmnd <- paste("aws s3 cp",paste(download_directory,file,sep=""), 
+                paste(s3_uri,file,sep=""),"--recursive")
+  system(cmnd)
+  
+  cmnd <- paste("rm -rf ",download_directory,"*",sep="")
+  system(cmnd)
 }
 
-# unzip the files and delete the .zip files
-for(file in file_list){
-  unzip(paste(download_directory,file,sep=""), exdir = download_directory, unzip="unzip")
-  file.remove(paste(download_directory,file,sep=""))
-}
+
